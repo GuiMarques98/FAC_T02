ae04b2a03555dbc82e480af9b6e5ad7e2f2b1d21
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-09-12 08:42:22 -0300

    Funções padroes pedidos

---
+++ inv_mult_mod.asm
@@ -0,0 +1,23 @@
+.data
+.text
+
+#Parte principal do codigo
+main:
+
+
+#Le numero inteiro
+r_interger:
+
+
+#Verifica se e um numero primo
+is_prime:
+
+#calcula o inverso multiplicativo modular
+calc_inv:
+
+
+#Imprime erros
+print_error:
+
+#Imprime saida
+print_out:
